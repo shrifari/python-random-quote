8903f0de76d615bbb55a057a9c733c17000cd47c
shrifari <shrifari622@gmail.com>
2020-01-12 19:50:01 -0600

    remove new lines

--- get-quote.py
+++ get-quote.py
@@ -4,10 +4,13 @@ def primary():
 
   f = open("quotes.txt")
   quotes = f.readlines()
+  newlist = []
+  for i in quotes:
+    newlist.append(i.rstrip('\n'))
   f.close()
   last = 13
   rnd = random.randint(0, last)
-  print(quotes[rnd])
+  print(newlist[rnd])
 
 if __name__== "__main__":
   primary()
